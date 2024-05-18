0f1a36f3bc9c9257816f066a2e2f65539d3d1c30
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-18 19:38:20 +0530

    Time: 3 ms (82.42%), Space: 16.5 MB (7.95%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs_m(TreeNode* root , TreeNode* prev,map<TreeNode*,TreeNode*> &m)
+    {
+        if(!root)
+        {
+            return;
+        }
+        
+        m[root] = prev;
+        
+        dfs_m(root -> left , root , m);
+        dfs_m(root -> right , root , m);
+    }
+    void dist_k(TreeNode* root , map<TreeNode*,TreeNode*>&par ,  unordered_map<TreeNode*, int>&vis , int count , int k , vector<int> &v)
+    {
+        // visited is necessary otherwise calls won't end
+        
+        if(!root)
+        {
+            return;
+        }
+
+        if(count == k)
+        {
+            v.push_back(root -> val);
+        }
+        
+        vis[root]=1;
+        
+        int lh= -1;
+        int rh= -1;
+        int ph= -1;
+        
+        if(!vis[root->left])
+        dist_k(root->left, par, vis , count + 1 , k , v);
+          
+        if(!vis[root->right])
+        dist_k(root->right, par, vis , count + 1 , k , v);
+        
+        if(!vis[par[root]])
+        dist_k(par[root] , par, vis , count + 1 , k , v);
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) 
+    {
+        map<TreeNode* , TreeNode*> m; // Parent Mapping
+
+        TreeNode* fnd = target;
+
+        TreeNode* prev = NULL;
+
+        dfs_m(root , prev, m);
+
+        vector <int> v;
+
+        unordered_map <TreeNode* , int> vis;
+        dist_k(fnd , m , vis , 0 , k , v);
+
+        return v;
+    }
+};
