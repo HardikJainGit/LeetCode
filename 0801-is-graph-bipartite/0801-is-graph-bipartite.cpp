533251e6621201a79253328ca2ff9f4d83eefaf0
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-21 21:31:04 +0530

    Time: 15 ms (81.69%), Space: 16.4 MB (39.66%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -7,6 +7,65 @@
 class Solution {
 public:
 
+    // Always pass adj by ref otherwise tle 
+
+    bool bfs(int i,vvi &graph,vi &vis,vi &color)
+    {    
+        queue<int> q;
+            
+        q.push(i);
+        vis[i] = true;
+        
+        while (!q.empty()) 
+        {
+            int v = q.front();
+            q.pop();
+            for (int u : graph[v]) 
+            {
+                if (!vis[u]) 
+                {
+                    vis[u] = true;
+                    q.push(u);
+                }
+                if(color[u] == -1)
+                {
+                    color[u] = !color[v];
+                }
+                if(color[u] != -1 && color[u] == color[v])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int v = sz(graph);
+        vi vis(v,0);
+
+        vi col(v,-1);
+
+        f(v)
+        {
+            if(!vis[i])
+            {
+                if(!bfs(i,graph,vis,col))
+                {
+                    col[i] = 0;
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+/* DFS : 
+class Solution {
+public:
+
     bool dfs(int node , vvi &graph , vi &vis , vi &color)
     {
         vis[node] = 1;
@@ -53,4 +112,5 @@ public:
 
         return true;
     }
-};
+}; 
+*/
