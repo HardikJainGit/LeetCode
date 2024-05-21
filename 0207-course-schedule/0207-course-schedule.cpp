ae767359a961c61df21c9e6b86798910b2704581
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-21 18:25:46 +0530

    Time: 123 ms (5.02%), Space: 70.9 MB (5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -21,59 +21,73 @@
 #define endl '\n'
 #define pr(x) cout<<x<<endl;
 
-bool isCyclicDFS(vvi adj, vector<int> &vis, vector<int> &vis_dfs, int Node) 
-{
-    vis[Node] = 1;
-    vis_dfs[Node] = 1;
-
-    for(int x : adj[Node]) 
-    {
-        if(!vis[x]) 
-        {
-            bool ans = isCyclicDFS(adj, vis, vis_dfs, x);
-
-            if(ans) 
-            {
-                return true;
-            }
-        } 
-        else if(vis_dfs[x]) 
-        {
-            return true; //vis true and dfs vis also true
-        }
-    }
-
-    vis_dfs[Node] = 0; //while returning back mark dfs vis false
-    return false;
-}
-
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    void Kahn(int i , vector<int> &topo, vvi adj, vector<int> &indegree, vector<int> &vis) 
     {
-        vvi adj(numCourses);
-        int v = numCourses;
-        
-        f(sz(prerequisites))
+        queue<int> q;
+
+        if(indegree[i] == 0 && !vis[i])
         {
-            adj[prerequisites[i][0]].pb(prerequisites[i][1]);
+            q.push(i);
+            vis[i] = 1;
         }
-
-        vi vis(v,0);
-        vi dfs_v(v,0);
-
-        for(int i=0; i<v; i++) 
+    
+        while (!q.empty()) 
         {
-            if(!vis[i]) 
+            int v = q.front();
+            q.pop();
+            
+            topo.push_back(v);
+            
+            for (int u : adj[v]) 
             {
-                int ans = isCyclicDFS(adj, vis, dfs_v, i);
-
-                if(ans) 
+                indegree[u]--;
+                
+                if (!vis[u] && !indegree[u]) 
                 {
-                    return false;
+                    vis[u] = 1;
+                    q.push(u);
                 }
             }
         }
-        return true;
+    }
+    
+	vector<int> topoSort(int V, vvi adj) 
+	{
+	    int v = V;
+	    vector<int> indegree(v,0);
+        vector<int> vis(v,0);
+        vector<int> topo;
+    
+        for (int i = 0; i < V; ++i)
+        {
+            for (int el : adj[i])
+            {
+                indegree[el]++;
+            }
+        }
+
+        for(int i=0; i<v; i++) 
+        {
+            if(!vis[i] && indegree[i] == 0) 
+            {
+                Kahn(i,topo, adj, indegree, vis);
+            }
+        }
+    
+        return topo;
+	}
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vvi adj(numCourses);
+        f(sz(prerequisites))
+        {
+            adj[prerequisites[i][1]].pb(prerequisites[i][0]);
+        }
+
+        vector<int> topo = topoSort(numCourses, adj);
+        
+        return sz(topo) == numCourses;
     }
 };
