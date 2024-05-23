9d8b4da34e17a464c5108f9f28498ac299b3bbfb
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-24 00:29:33 +0530

    Update 1753-path-with-minimum-effort.cpp

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -5,23 +5,6 @@
 #define f(b) for(int i=0;i<b;i++)
 #define fz(i,a,b) for(int i=a;i<b;i++)
 #define sz(x) ((int)(x).size())
-#define vp vector<pair<int,int>>
-#define pi pair<int,int>
-#define pb push_back
-#define ff first
-#define ss second
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
-#define rev(v) reverse(v.begin(), v.end())
-#define srt(v) sort(v.begin(), v.end())
-#define minv(v) *min_element(v.begin(), v.end())
-#define maxv(v) *max_element(v.begin(), v.end())
-#define all(v) v.begin(),v.end()
-#define in(x) int x; cin>>x;
-#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
-#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
-#define endl '\n'
-#define pr(x) cout<<x<<endl;
 
 class Solution {
 public:
