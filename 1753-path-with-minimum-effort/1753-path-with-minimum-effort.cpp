b03ffa9c8660e74e373035c66e7cf88f4acacd7e
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-24 23:53:00 +0530

    Time: 134 ms (29.59%), Space: 43.7 MB (20.65%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,5 +1,6 @@
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
+// For DFS + Binary search ; binary search on the variable heights and check if we can reach or not. For dijkstra use the standard algorithm with the below modification.
+// For Union Find ; it is greedy basically pick smallest weights first and keep unifying corresponding cells until 0 and the last cells unite ; when they unite return the current height difference. Voila !
+
 #define vi vector <int>
 #define vvi vector<vector<int>>
 #define vp vector<pair<int,int>>
@@ -7,142 +8,129 @@
 #define pb push_back
 #define ff first
 #define ss second
-#define rev(v) reverse(v.begin(), v.end())
-#define srt(v) sort(v.begin(), v.end())
-#define minv(v) *min_element(v.begin(), v.end())
-#define maxv(v) *max_element(v.begin(), v.end())
-#define all(v) v.begin(),v.end()
-#define in(x) int x; cin>>x;
 #define f(b) for(int i=0;i<b;i++)
 #define fz(i,a,b) for(int i=a;i<b;i++)
-#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
-#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
-#define endl '\n'
-#define pr(x) cout<<x<<endl;
 #define sz(x) ((int)(x).size())
 
-// class Solution {
-// public:
-//     bool dfs(int mid, vvi &heights, int i, int j, vvi &vis) 
-//     {
-//         int m = sz(heights);
-//         int n = sz(heights[0]);
+class Solution {
+public:
+    bool dfs(int mid, vvi &heights, int i, int j, vvi &vis) 
+    {
+        int m = sz(heights);
+        int n = sz(heights[0]);
 
-//         if (i == m - 1 && j == n - 1) 
-//         {
-//             return true;
-//         }
+        if (i == m - 1 && j == n - 1) 
+        {
+            return true;
+        }
 
-//         if (i < 0 || i >= m || j < 0 || j >= n || vis[i][j]) 
-//         {
-//             return false;
-//         }
+        if (i < 0 || i >= m || j < 0 || j >= n || vis[i][j]) 
+        {
+            return false;
+        }
 
-//         vis[i][j] = 1;
-// 
-        // int drow[] = {-1, 1, 0, 0};
-//         int dcol[] = {0, 0, -1, 1};
+        vis[i][j] = 1;
 
-//         fz(k, 0, 4) 
-//         {
-//             int nrow = i + drow[k];
-//             int ncol = j + dcol[k];
+        int drow[] = {-1, 1, 0, 0};
+        int dcol[] = {0, 0, -1, 1};
 
-//             if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !vis[nrow][ncol]) 
-//             {
-//                 if (abs(heights[nrow][ncol] - heights[i][j]) <= mid) 
-//                 {
-//                     if (dfs(mid, heights, nrow, ncol, vis)) 
-//                     {
-//                         return true;
-//                     }
-//                 }
-//             }
-//         }
-//         return false;
-//     }
+        fz(k, 0, 4) 
+        {
+            int nrow = i + drow[k];
+            int ncol = j + dcol[k];
 
-//     int minimumEffortPath(vector<vector<int>>& heights) 
-//     {
-//         int m = sz(heights);
-//         int n = sz(heights[0]);
+            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !vis[nrow][ncol]) 
+            {
+                if (abs(heights[nrow][ncol] - heights[i][j]) <= mid) 
+                {
+                    if (dfs(mid, heights, nrow, ncol, vis)) 
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
-//         if(!m && !n)
-//         {
-//             return 0;
-//         }
+    int minimumEffortPath(vector<vector<int>>& heights) 
+    {
+        int m = sz(heights);
+        int n = sz(heights[0]);
 
-//         int lo = -1;
-//         int hi = 1e6 + 1;
-//         int mid = 0;
+        if(!m && !n)
+        {
+            return 0;
+        }
+
+        int lo = -1;
+        int hi = 1e6 + 1;
+        int mid = 0;
         
-//         while (hi - lo > 1)
-//         {
-//             vvi vis(m,vi(n,0));
-//             mid = (hi + lo) / 2;
+        while (hi - lo > 1)
+        {
+            vvi vis(m,vi(n,0));
+            mid = (hi + lo) / 2;
     
-//             if (dfs(mid,heights,0,0,vis)) // FFFFTTTT
-//             {
-//                 hi = mid;
-//             }
-//             else
-//             {
-//                 lo = mid;
-//             }
-//         }
+            if (dfs(mid,heights,0,0,vis)) // FFFFTTTT
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid;
+            }
+        }
 
-//         return hi;
-//     }   
-// };
+        return hi;
+    }   
 
-// class Solution {
-// public:
-//     int minimumEffortPath(vector<vector<int>>& heights) 
-//     {
-//         int n = heights.size();
-//         int m = heights[0].size();
+    /* int minimumEffortPath(vector<vector<int>>& heights) 
+    {
+        int n = heights.size();
+        int m = heights[0].size();
         
-//         vvi d(n, vi(m, INT_MAX));
-//         d[0][0] = 0;
+        vvi d(n, vi(m, INT_MAX));
+        d[0][0] = 0;
         
-//         int delrow[] = {-1, 0, 1, 0}; 
-// 	    int delcol[] = {0, 1, 0, -1}; 
+        int delrow[] = {-1, 0, 1, 0}; 
+	    int delcol[] = {0, 1, 0, -1}; 
         
-//         set<pair<int, pi>> q;
-//         q.insert({0, {0,0}});
+        set<pair<int, pi>> q;
+        q.insert({0, {0,0}});
         
-//         while (!q.empty()) 
-//         {
-//             int row = (*q.begin()).ss.ff;
-//             int col = (*q.begin()).ss.ss;
-//             int ef = (*q.begin()).ff;
-//             q.erase(q.begin());
+        while (!q.empty()) 
+        {
+            int row = (*q.begin()).ss.ff;
+            int col = (*q.begin()).ss.ss;
+            int ef = (*q.begin()).ff;
+            q.erase(q.begin());
 
-//             if(row == n-1 && col == m-1)
-//             {
-//                 return d[row][col];
-//             }
+            if(row == n-1 && col == m-1)
+            {
+                return d[row][col];
+            }
 
-//             for (int i = 0; i < 4; i++) 
-//             {
-//                 int nrow = row + delrow[i]; 
-//                 int ncol = col + delcol[i]; 
+            for (int i = 0; i < 4; i++) 
+            {
+                int nrow = row + delrow[i]; 
+                int ncol = col + delcol[i]; 
                     
-//                 if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) 
-//                 {
-//                     int mx = max(ef, abs(heights[nrow][ncol] - heights[row][col]));
-//                     if(mx < d[nrow][ncol]) 
-//                     {
-//                         d[nrow][ncol] = mx;
-//                         q.insert({mx,{nrow, ncol}});  
-//                     }
-//                 }
-//             }
-//         }
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) 
+                {
+                    int mx = max(ef, abs(heights[nrow][ncol] - heights[row][col]));
+                    if(mx < d[nrow][ncol]) 
+                    {
+                        d[nrow][ncol] = mx;
+                        q.insert({mx,{nrow, ncol}});  
+                    }
+                }
+            }
+        }
         
-//         return -1;
-//     }
-// };
+        return -1;
+    }
+
 struct DSU
 {
     int n;
@@ -166,8 +154,7 @@ struct DSU
         p[v] = u;
     }
 };
-class Solution {
-public:
+
     int minimumEffortPath(vector<vector<int>>& heights) 
     {
         if(heights.empty())
@@ -212,5 +199,5 @@ public:
         }
        return 0;
 
-    }
+    } */
 };
