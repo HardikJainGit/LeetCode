91e2aad12a41393dfda64cb33660b4c688d283a7
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-25 03:39:29 +0530

    Time: 21 ms (6.22%), Space: 19.5 MB (6%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,167 +1,94 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void __print(int x) { cout << x; }
-void __print(long x) { cout << x; }
-void __print(long long x) { cout << x; }
-void __print(unsigned x) { cout << x; }
-void __print(unsigned long x) { cout << x; }
-void __print(unsigned long long x) { cout << x; }
-void __print(float x) { cout << x; }
-void __print(double x) { cout << x; }
-void __print(long double x) { cout << x; }
-void __print(char x) { cout << '\'' << x << '\''; }
-void __print(const char *x) { cout << '\"' << x << '\"'; }
-void __print(const string &x) { cout << '\"' << x << '\"'; }
-void __print(bool x) { cout << (x ? "true" : "false"); }
-template <typename A> void __print(const A &x);
-template <typename A, typename B> void __print(const pair<A, B> &p);
-template <typename... A> void __print(const tuple<A...> &t);
-template <typename T> void __print(stack<T> s);
-template <typename T> void __print(queue<T> q);
-template <typename T, typename... U> void __print(priority_queue<T, U...> q);
-template <typename A> void __print(const A &x) {
-    bool first = true;
-    cout << '{';
-    for (const auto &i : x) {
-        cout << (first ? "" : ","), __print(i);
-        first = false;
-    }
-    cout << '}';
-}
-template <typename A, typename B> void __print(const pair<A, B> &p) {
-    cout << '(';
-    __print(p.first);
-    cout << ',';
-    __print(p.second);
-    cout << ')';
-}
-template <typename... A> void __print(const tuple<A...> &t) {
-    bool first = true;
-    cout << '(';
-    apply([&first](const auto &...args) { ((cout << (first ? "" : ","), __print(args), first = false), ...); }, t);
-    cout << ')';
-}
-template <typename T> void __print(stack<T> s) {
-    vector<T> debugVector;
-    while (!s.empty()) {
-        T t = s.top();
-        debugVector.push_back(t);
-        s.pop();
-    }
-    reverse(debugVector.begin(), debugVector.end());
-    __print(debugVector);
-}
-template <typename T> void __print(queue<T> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.front();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.top();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-void _print() { cout << "]"<<endl; }
-template <typename Head, typename... Tail> void _print(const Head &H, const Tail &...T) {
-    __print(H);
-    if (sizeof...(T)) cout << ", ";
-    _print(T...);
-}
-#define debug(...) cout << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
-
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
-#define vi vector <int>
-#define vvi vector<vector<int>>
-#define vp vector<pair<int,int>>
-#define pi pair<int,int>
-#define pb push_back
-#define ff first
-#define ss second
-#define rev(v) reverse(v.begin(), v.end())
-#define srt(v) sort(v.begin(), v.end())
-#define minv(v) *min_element(v.begin(), v.end())
-#define maxv(v) *max_element(v.begin(), v.end())
-#define all(v) v.begin(),v.end()
-#define in(x) int x; cin>>x;
-#define f(b) for(int i=0;i<b;i++)
-#define fz(i,a,b) for(int i=a;i<b;i++)
-#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
-#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
-#define endl '\n'
-#define pr(x) cout<<x<<endl;
-#define sz(x) ((int)(x).size())
-
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n = sz(strs);
-        string sm = minv(strs);
-        int k = sm.length();
-        int f = 1;
-        int jf = -1;
 
-        if(n==1)
-        {
-            return strs[0];
-        }
+class TrieNode {
+    public:
+        char data;
+        TrieNode* children[26];
+        bool isTerminal;
+        int childCount;
 
-        fz(j,0,k)
-        {
-            f(n-1)
-            {
-                if(strs[i][j] == strs[i+1][j])
-                {
-                    continue;
-                }
-                else
-                {
-                    jf = j;
-                    f=0;
-                    break;
-                }
-            }
-            if(!f)
-            {
-                break;
+        TrieNode(char data) {
+            this->data = data;
+            this->isTerminal = false;
+            this->childCount = 0;
+
+            for(int i=0; i<26; i++) {
+                this->children[i] = NULL;
             }
         }
+};
+
+class Trie {
+    public:
+        TrieNode* root=new TrieNode('\0');
+
+        // Trie() {
+        //     root = new TrieNode('\0');
+        // }
+
+        void insertUtil(TrieNode* root, string word) {
+            if(word.length() == 0) {
+                root->isTerminal = true;
+                return;
+            }
+
+            // Assuming the string is in lowercase
+            int index = word[0] - 'a';
+            TrieNode* child;
+
+            if(root->children[index] == NULL) { 
+                child = new TrieNode(word[0]);
+                root->childCount++;
+                root->children[index] = child;
+            } else {
+                child = root->children[index]; //update child 
+            }
+
+            insertUtil(child, word.substr(1));
+        }
+
+        void insertNode(string word) {
+            insertUtil(root, word);
+        }
+};
+
+/* TC and SC both (O(M*N)) */
+
+string longestCommonPrefix(vector<string>& strs) 
+{
+    string ans = "";
+    Trie* t = new Trie();
+
+    // insert all strings in trie;
+    for(int i=0; i<strs.size(); i++) 
+    {
+        if(strs[i] == "")
+        {
+            return "";
+        }
+        t->insertNode(strs[i]);
+    }
+
+    TrieNode* temp = t->root;
+
+    for(int i=0; i<strs[0].length(); i++) {
+        if(temp->childCount == 1) {
+            ans.push_back(strs[0][i]); //push chars with only one child
+
+            int index = strs[0][i] - 'a';
+            temp = temp->children[index]; //update temp
+        } 
         
-        debug(jf);
-
-        if(jf==0)
-        {
-            f(n-1)
-            {
-                if(strs[i][0] == strs[i+1][0])
-                {
-                    continue;
-                }
-                else
-                {
-                    f=0;
-                    break;
-                }
-            }
+        else {
+            break;
         }
 
-
-        string res(1,sm[0]);
-
-        if(jf==0 && f)
-        {
-            return res;
+        if(temp->isTerminal) {  //for 2 words AGE and A ans will be 2 but we need 1
+            break;
         }
+    }
 
-        return sm.substr(0,jf);
+    return ans;
     }
 };
