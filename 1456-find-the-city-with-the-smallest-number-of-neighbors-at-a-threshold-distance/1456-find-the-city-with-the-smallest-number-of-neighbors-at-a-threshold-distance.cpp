51482990fe098deefa77084b92787fe6075c47e4
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-24 16:21:35 +0530

    Update 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,158 +1,94 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void __print(int x) { cout << x; }
-void __print(long x) { cout << x; }
-void __print(long long x) { cout << x; }
-void __print(unsigned x) { cout << x; }
-void __print(unsigned long x) { cout << x; }
-void __print(unsigned long long x) { cout << x; }
-void __print(float x) { cout << x; }
-void __print(double x) { cout << x; }
-void __print(long double x) { cout << x; }
-void __print(char x) { cout << '\'' << x << '\''; }
-void __print(const char *x) { cout << '\"' << x << '\"'; }
-void __print(const string &x) { cout << '\"' << x << '\"'; }
-void __print(bool x) { cout << (x ? "true" : "false"); }
-template <typename A> void __print(const A &x);
-template <typename A, typename B> void __print(const pair<A, B> &p);
-template <typename... A> void __print(const tuple<A...> &t);
-template <typename T> void __print(stack<T> s);
-template <typename T> void __print(queue<T> q);
-template <typename T, typename... U> void __print(priority_queue<T, U...> q);
-template <typename A> void __print(const A &x) {
-    bool first = true;
-    cout << '{';
-    for (const auto &i : x) {
-        cout << (first ? "" : ","), __print(i);
-        first = false;
-    }
-    cout << '}';
-}
-template <typename A, typename B> void __print(const pair<A, B> &p) {
-    cout << '(';
-    __print(p.first);
-    cout << ',';
-    __print(p.second);
-    cout << ')';
-}
-template <typename... A> void __print(const tuple<A...> &t) {
-    bool first = true;
-    cout << '(';
-    apply([&first](const auto &...args) { ((cout << (first ? "" : ","), __print(args), first = false), ...); }, t);
-    cout << ')';
-}
-template <typename T> void __print(stack<T> s) {
-    vector<T> debugVector;
-    while (!s.empty()) {
-        T t = s.top();
-        debugVector.push_back(t);
-        s.pop();
-    }
-    reverse(debugVector.begin(), debugVector.end());
-    __print(debugVector);
-}
-template <typename T> void __print(queue<T> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.front();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.top();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-void _print() { cout << "]"<<endl; }
-template <typename Head, typename... Tail> void _print(const Head &H, const Tail &...T) {
-    __print(H);
-    if (sizeof...(T)) cout << ", ";
-    _print(T...);
-}
-#define debug(...) cout << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
-
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
 #define vi vector <int>
 #define vvi vector<vector<int>>
+#define vvvi vector<vector<vi>>
 #define vp vector<pair<int,int>>
 #define pi pair<int,int>
 #define pb push_back
 #define ff first
 #define ss second
-#define rev(v) reverse(v.begin(), v.end())
-#define srt(v) sort(v.begin(), v.end())
-#define minv(v) *min_element(v.begin(), v.end())
-#define maxv(v) *max_element(v.begin(), v.end())
-#define all(v) v.begin(),v.end()
-#define in(x) int x; cin>>x;
 #define f(b) for(int i=0;i<b;i++)
 #define fz(i,a,b) for(int i=a;i<b;i++)
-#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
-#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
-#define endl '\n'
-#define pr(x) cout<<x<<endl;
 #define sz(x) ((int)(x).size())
 
 class Solution {
 public:
 #define INF 99999
+    void dijkstra(int s, vector<int>& d, vvvi &adj) 
+    {
+        int n = d.size();
+        d.assign(n, INT_MAX);
+        vector<bool> vis(n, false);
 
-    void shortest_distance(vector<vector<int>>& matrix) {
+        d[s] = 0;
+        
+        set<pair<int, int>> q;
+        q.insert({0, s});
+        while (!q.empty()) 
+        {
+            int v = (*q.begin()).second;
+            q.erase(q.begin());
+
+            for (int j = 0; j < adj[v].size(); j++) 
+            {
+                int to = adj[v][j][0];
+                int len = adj[v][j][1];
+                if (d[v] + len < d[to]) 
+                {
+                    d[to] = d[v] + len;
+                    q.insert({d[to], to});
+                    // p[to] = v;
+                }
+            }
+        }
+    }
+    void floyd(vector<vector<int>>& matrix) 
+    {
         int V = matrix.size();
-
-        // Replace -1 with INF, and set diagonal elements to 0
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
-                if (matrix[i][j] == -1 && i != j) {
-                    matrix[i][j] = INF;
-                } else if (i == j) {
+        for (int i = 0; i < V; ++i) 
+        {
+            for (int j = 0; j < V; ++j) 
+            {
+                if (i == j) 
+                {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // Floyd-Warshall algorithm
-        // k is split point i starting j ending
-        for (int k = 0; k < V; ++k) {
-            for (int i = 0; i < V; ++i) {
-                for (int j = 0; j < V; ++j) {
-                    if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {
+        for (int k = 0; k < V; ++k) 
+        {
+            for (int i = 0; i < V; ++i) 
+            {
+                for (int j = 0; j < V; ++j) 
+                {
+                    if (matrix[i][j] > matrix[i][k] + matrix[k][j]) 
+                    {
                         matrix[i][j] = matrix[i][k] + matrix[k][j];
                     }
-                }
-            }
-        }
-
-        // Replace INF back to -1 to indicate no path
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
-                if (matrix[i][j] == INF) {
-                    matrix[i][j] = -1;
                 }
             }
         }
     }
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) 
     {
-        vvi d(n,vi(n,-1));
+        vvi d(n,vi(n,1e9));
+        vvvi adj(n);
         unordered_map<int,int> m;
 
         for(auto it : edges)
         {
-            d[it[0]][it[1]] = it[2];
-            d[it[1]][it[0]] = it[2];
+            adj[it[0]].pb({it[1],it[2]});
+            adj[it[1]].pb({it[0],it[2]});
         }
 
-        shortest_distance(d);
-        debug(d);
+        // floyd(d);
+
+        f(n)
+        {
+            dijkstra(i,d[i],adj);
+        }
+
+        // debug(d);
 
         f(n)
         {
@@ -168,7 +104,7 @@ public:
         int mi = INT_MAX;
         int ret = INT_MIN;
 
-        debug(m);
+        // debug(m);
 
         for(auto it : m)
         {
