dc76f6bb961aa1098f91bc9d50037f6e5db9ed00
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-05-24 23:43:39 +0530

    Time: 273 ms (10.1%), Space: 75.3 MB (7.02%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,11 +1,24 @@
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
 #define vi vector <int>
 #define vvi vector<vector<int>>
+#define vp vector<pair<int,int>>
 #define pi pair<int,int>
 #define pb push_back
 #define ff first
 #define ss second
+#define rev(v) reverse(v.begin(), v.end())
+#define srt(v) sort(v.begin(), v.end())
+#define minv(v) *min_element(v.begin(), v.end())
+#define maxv(v) *max_element(v.begin(), v.end())
+#define all(v) v.begin(),v.end()
+#define in(x) int x; cin>>x;
 #define f(b) for(int i=0;i<b;i++)
 #define fz(i,a,b) for(int i=a;i<b;i++)
+#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
+#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
+#define endl '\n'
+#define pr(x) cout<<x<<endl;
 #define sz(x) ((int)(x).size())
 
 // class Solution {
@@ -26,8 +39,8 @@
 //         }
 
 //         vis[i][j] = 1;
-
-//         int drow[] = {-1, 1, 0, 0};
+// 
+        // int drow[] = {-1, 1, 0, 0};
 //         int dcol[] = {0, 0, -1, 1};
 
 //         fz(k, 0, 4) 
@@ -82,50 +95,122 @@
 //     }   
 // };
 
+// class Solution {
+// public:
+//     int minimumEffortPath(vector<vector<int>>& heights) 
+//     {
+//         int n = heights.size();
+//         int m = heights[0].size();
+        
+//         vvi d(n, vi(m, INT_MAX));
+//         d[0][0] = 0;
+        
+//         int delrow[] = {-1, 0, 1, 0}; 
+// 	    int delcol[] = {0, 1, 0, -1}; 
+        
+//         set<pair<int, pi>> q;
+//         q.insert({0, {0,0}});
+        
+//         while (!q.empty()) 
+//         {
+//             int row = (*q.begin()).ss.ff;
+//             int col = (*q.begin()).ss.ss;
+//             int ef = (*q.begin()).ff;
+//             q.erase(q.begin());
+
+//             if(row == n-1 && col == m-1)
+//             {
+//                 return d[row][col];
+//             }
+
+//             for (int i = 0; i < 4; i++) 
+//             {
+//                 int nrow = row + delrow[i]; 
+//                 int ncol = col + delcol[i]; 
+                    
+//                 if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) 
+//                 {
+//                     int mx = max(ef, abs(heights[nrow][ncol] - heights[row][col]));
+//                     if(mx < d[nrow][ncol]) 
+//                     {
+//                         d[nrow][ncol] = mx;
+//                         q.insert({mx,{nrow, ncol}});  
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return -1;
+//     }
+// };
+struct DSU
+{
+    int n;
+    vi p;
+    DSU(int n)
+    {
+        p.assign(n + 1,0);
+        fz(i,1,n+1)
+            p[i] = i;
+    }
+    int par(int i)
+    {
+        if (p[i] == i)
+            return i;
+        return p[i] = par(p[i]);
+    }
+    void un(int u,int v)
+    {
+        u = par(u),v = par(v);
+        if(u==v) return;
+        p[v] = u;
+    }
+};
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) 
     {
-        int n = heights.size();
-        int m = heights[0].size();
-        
-        vvi d(n, vi(m, INT_MAX));
-        d[0][0] = 0;
-        
-        int delrow[] = {-1, 0, 1, 0}; 
-	    int delcol[] = {0, 1, 0, -1}; 
-        
-        set<pair<int, pi>> q;
-        q.insert({0, {0,0}});
-        while (!q.empty()) 
+        if(heights.empty())
         {
-            int row = (*q.begin()).ss.ff;
-            int col = (*q.begin()).ss.ss;
-            int ef = (*q.begin()).ff;
-            q.erase(q.begin());
+            return 0;
+        }
 
-            if(row == n-1 && col == m-1)
-            {
-                return d[row][col];
-            }
+        int r = sz(heights);
+        int c = sz(heights[0]);
+        set<pair<int,pi>> q;
 
-            f(4)
+        DSU dsu(r * c);
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        f(r) 
+        {
+            fz(j, 0, c) 
             {
-                int nrow = row + delrow[i]; 
-                int ncol = col + delcol[i]; 
-                    
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) 
+                fz(k, 0, 4) 
                 {
-                    int mx = max(ef, abs(heights[nrow][ncol] - heights[row][col]));
-                    if(mx < d[nrow][ncol]) 
+                    int nr = i + delRow[k], nc = j + delCol[k];
+                    if (nr >= 0 && nr < r && nc >= 0 && nc < c) 
                     {
-                        d[nrow][ncol] = mx;
-                        q.insert({mx,{nrow, ncol}});  
+                        q.insert({abs(heights[i][j] - heights[nr][nc]),{i * c + j, nr * c + nc}});
                     }
                 }
             }
         }
-        
-        return -1;
+        for (auto it : q)
+        {
+            int w = it.ff;
+            int u = it.ss.ff;
+            int v = it.ss.ss;
+
+            dsu.un(u, v);
+
+            if (dsu.par(0) == dsu.par((r-1)*c + c-1)) 
+            {
+                return w;
+            }
+        }
+       return 0;
+
     }
 };
