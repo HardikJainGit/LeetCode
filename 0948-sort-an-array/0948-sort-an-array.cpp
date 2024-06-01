cd7d7ac6e232666553a4563da0c3b93e169161d6
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2024-06-01 19:18:42 +0530

    Time: 122 ms (70.99%), Space: 69.1 MB (88.25%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void maxHeapify(vector<int>& nums, int n, int i)
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        
+        if(l<n && nums[l] > nums[largest])
+            largest = l;
+        if(r<n && nums[r] > nums[largest])
+            largest = r;
+        
+        if(largest != i)
+        {
+            swap(nums[largest], nums[i]);
+            maxHeapify(nums,n,largest);
+        }
+    }
+    
+    void buildHeap(vector<int>& nums, int n)
+    {
+        for(int i=n/2 - 1;i>=0;i--)
+            maxHeapify(nums,n,i);
+    }
+    
+    void heapSort(vector<int>& nums, int n)
+    {
+        buildHeap(nums,n);
+
+        // keep swapping first and last
+        // keep reducing size that we pass to buildHeap
+        
+        for(int i=n-1;i>=1;i--)
+        {
+            swap(nums[0],nums[i]);
+            maxHeapify(nums,i,0);
+        }
+    }
+    
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        heapSort(nums,nums.size());
+        
+        return nums;
+    }
+};
