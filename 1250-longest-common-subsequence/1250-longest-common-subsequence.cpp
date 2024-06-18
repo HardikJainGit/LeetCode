df28e8199bbd06c831c901c0956f0ff37bacaf0c
Hardik Jain <133627261+HardikJainGit@users.noreply.github.com>
2024-06-18 17:38:15 +0530

    Time: 35 ms (30.18%), Space: 24.8 MB (76.52%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,140 +1,128 @@
-/*
- * @lc app=leetcode id=1143 lang=cpp
- *
- * [1143] Longest Common Subsequence
- */
-
-// @lc code=start
-#include <bits/stdc++.h>
-using namespace std;
-
-void __print(int x) { cout << x; }
-void __print(long x) { cout << x; }
-void __print(long long x) { cout << x; }
-void __print(unsigned x) { cout << x; }
-void __print(unsigned long x) { cout << x; }
-void __print(unsigned long long x) { cout << x; }
-void __print(float x) { cout << x; }
-void __print(double x) { cout << x; }
-void __print(long double x) { cout << x; }
-void __print(char x) { cout << '\'' << x << '\''; }
-void __print(const char *x) { cout << '\"' << x << '\"'; }
-void __print(const string &x) { cout << '\"' << x << '\"'; }
-void __print(bool x) { cout << (x ? "true" : "false"); }
-template <typename A> void __print(const A &x);
-template <typename A, typename B> void __print(const pair<A, B> &p);
-template <typename... A> void __print(const tuple<A...> &t);
-template <typename T> void __print(stack<T> s);
-template <typename T> void __print(queue<T> q);
-template <typename T, typename... U> void __print(priority_queue<T, U...> q);
-template <typename A> void __print(const A &x) {
-    bool first = true;
-    cout << '{';
-    for (const auto &i : x) {
-        cout << (first ? "" : ","), __print(i);
-        first = false;
-    }
-    cout << '}';
-}
-template <typename A, typename B> void __print(const pair<A, B> &p) {
-    cout << '(';
-    __print(p.first);
-    cout << ',';
-    __print(p.second);
-    cout << ')';
-}
-template <typename... A> void __print(const tuple<A...> &t) {
-    bool first = true;
-    cout << '(';
-    apply([&first](const auto &...args) { ((cout << (first ? "" : ","), __print(args), first = false), ...); }, t);
-    cout << ')';
-}
-template <typename T> void __print(stack<T> s) {
-    vector<T> debugVector;
-    while (!s.empty()) {
-        T t = s.top();
-        debugVector.push_back(t);
-        s.pop();
-    }
-    reverse(debugVector.begin(), debugVector.end());
-    __print(debugVector);
-}
-template <typename T> void __print(queue<T> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.front();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.top();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
-}
-void _print() { cout << "]"<<endl; }
-template <typename Head, typename... Tail> void _print(const Head &H, const Tail &...T) {
-    __print(H);
-    if (sizeof...(T)) cout << ", ";
-    _print(T...);
-}
-#define debug(...) cout << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
-
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
-#define vi vector <int>
-#define vvi vector<vector<int>>
-#define vp vector<pair<int,int>>
-#define pi pair<int,int>
-#define pb push_back
-#define ff first
-#define ss second
-#define rev(v) reverse(v.begin(), v.end())
-#define srt(v) sort(v.begin(), v.end())
-#define minv(v) *min_element(v.begin(), v.end())
-#define maxv(v) *max_element(v.begin(), v.end())
-#define all(v) v.begin(),v.end()
-#define in(x) int x; cin>>x;
-#define f(b) for(int i=0;i<b;i++)
-#define fz(i,a,b) for(int i=a;i<b;i++)
-#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
-#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
-#define endl '\n'
-#define pr(x) cout<<x<<endl;
-#define sz(x) ((int)(x).size())
-
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
 
-        int n1 = text1.length();
-        int n2 = text2.length();
-        vvi dp(n1+1,vi(n2+1,0));
+    int solve(string& a, string& b, int i, int j){
 
-        for(int i=n1-1;i>=0;i--)
+        // Base Case
+        if(i == a.length())
+            return 0;
+
+        if(j == b.length())
+            return 0;
+
+        int ans = 0;
+
+        if(a[i] == b[j])
+            ans = 1 + solve(a, b, i+1, j+1);
+
+        else
         {
-            for(int j=n2-1;j>=0;j--)
-            {
-                if(text1[i] == text2[j])
-                {
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i+1][j] , dp[i][j+1]);
-                }
-            }
+            ans = max(solve(a, b, i+1, j), solve(a, b, i, j+1));
         }
 
-        return dp[0][0];
+        return ans;
+    }
 
+    int solveMem(string& a, string& b, int i, int j, vector<vector<int>> &dp){
+
+        // Base Case
+        if(i == a.length())
+            return 0;
+
+        if(j == b.length())
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = 0;
+
+        if(a[i] == b[j])
+            ans = 1 + solveMem(a, b, i+1, j+1, dp);
+
+        else
+        {
+            ans = max(solveMem(a, b, i+1, j, dp), solveMem(a, b, i, j+1, dp));
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    int solveTab(string& a, string& b){
+
+        int n1 = a.length();
+        int n2 = b.length();
+
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
+
+        for(int i = n1-1; i>=0; i--){
+            for(int j = n2-1; j>=0; j--){
+                int ans = 0;
+
+                if(a[i] == b[j])
+                    ans = 1 + dp[i+1][j+1];
+
+                else
+                {
+                    ans = max(dp[i+1][j], dp[i][j+1]);
+                }
+
+                dp[i][j] = ans;
+            }
+        }
         
+        return dp[0][0];
+    }
+
+    int solveSpaceOP(string& a, string& b){
+
+        int n1 = a.length();
+        int n2 = b.length();
+
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
+
+        vector<int> currRow(n2+1, 0);
+        vector<int> nextRow(n2+1, 0);
+
+        for(int i = n1-1; i>=0; i--){
+            for(int j=n2-1;j>=0;j--){
+                int ans = 0;
+
+                if(a[i] == b[j]) //due to this condition cant reverse j in this case
+                    ans = 1 + nextRow[j+1];
+
+                else
+                {
+                    ans = max(nextRow[j], currRow[j+1]);
+                }
+
+                currRow[j] = ans;
+            }
+
+            nextRow = currRow;
+        }
+        
+        return nextRow[0];
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        // RECURSION
+        // return solve(text1, text2, 0, 0);
+
+        // RECURSION + MEMOIZATION
+        /*
+        int n1 = text1.length();
+        int n2 = text2.length();
+        
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, -1));
+
+        return solveMem(text1, text2, 0, 0, dp);
+        */
+
+        // TABULATION
+        // return solveTab(text1, text2);
+
+        // SPACE OPTIMIZATION
+        return solveSpaceOP(text1, text2);
     }
 };
-// @lc code=end
-
