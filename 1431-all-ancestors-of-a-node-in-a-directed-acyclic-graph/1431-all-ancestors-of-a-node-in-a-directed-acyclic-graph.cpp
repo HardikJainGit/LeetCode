d75e999ec91a6505771367bab4f3c81c4a198d45
Hardik Jain <133627261+HardikJainGit@users.noreply.github.com>
2024-06-29 15:27:38 +0530

    Time: 127 ms (61.25%), Space: 143.4 MB (43.03%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void __print(int x) { cout << x; }
+void __print(long x) { cout << x; }
+void __print(long long x) { cout << x; }
+void __print(unsigned x) { cout << x; }
+void __print(unsigned long x) { cout << x; }
+void __print(unsigned long long x) { cout << x; }
+void __print(float x) { cout << x; }
+void __print(double x) { cout << x; }
+void __print(long double x) { cout << x; }
+void __print(char x) { cout << '\'' << x << '\''; }
+void __print(const char *x) { cout << '\"' << x << '\"'; }
+void __print(const string &x) { cout << '\"' << x << '\"'; }
+void __print(bool x) { cout << (x ? "true" : "false"); }
+template <typename A> void __print(const A &x);
+template <typename A, typename B> void __print(const pair<A, B> &p);
+template <typename... A> void __print(const tuple<A...> &t);
+template <typename T> void __print(stack<T> s);
+template <typename T> void __print(queue<T> q);
+template <typename T, typename... U> void __print(priority_queue<T, U...> q);
+template <typename A> void __print(const A &x) {
+    bool first = true;
+    cout << '{';
+    for (const auto &i : x) {
+        cout << (first ? "" : ","), __print(i);
+        first = false;
+    }
+    cout << '}';
+}
+template <typename A, typename B> void __print(const pair<A, B> &p) {
+    cout << '(';
+    __print(p.first);
+    cout << ',';
+    __print(p.second);
+    cout << ')';
+}
+template <typename... A> void __print(const tuple<A...> &t) {
+    bool first = true;
+    cout << '(';
+    apply([&first](const auto &...args) { ((cout << (first ? "" : ","), __print(args), first = false), ...); }, t);
+    cout << ')';
+}
+template <typename T> void __print(stack<T> s) {
+    vector<T> debugVector;
+    while (!s.empty()) {
+        T t = s.top();
+        debugVector.push_back(t);
+        s.pop();
+    }
+    reverse(debugVector.begin(), debugVector.end());
+    __print(debugVector);
+}
+template <typename T> void __print(queue<T> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.front();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.top();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+void _print() { cout << "]"<<endl; }
+template <typename Head, typename... Tail> void _print(const Head &H, const Tail &...T) {
+    __print(H);
+    if (sizeof...(T)) cout << ", ";
+    _print(T...);
+}
+#define debug(...) cout << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define vi vector <int>
+#define vvi vector<vector<int>>
+#define vp vector<pair<int,int>>
+#define pi pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define rev(v) reverse(v.begin(), v.end())
+#define srt(v) sort(v.begin(), v.end())
+#define minv(v) *min_element(v.begin(), v.end())
+#define maxv(v) *max_element(v.begin(), v.end())
+#define all(v) v.begin(),v.end()
+// #define in(x) int x; cin>>x;
+#define f(b) for(int i=0;i<b;i++)
+#define fz(i,a,b) for(int i=a;i<b;i++)
+#define inp(a,n) int a[n]; fz(i,0,n) cin>>a[i];
+#define inpv(a,n) vector <int> a; fz(i,0,n) {int x; cin>>x; a.pb(x);}
+#define endl '\n'
+#define pr(x) cout<<x<<endl;
+#define sz(x) ((int)(x).size())
+
+
+class Solution {
+public:
+    void dfs(vi &a,int node,vvi &adj,vi &vis)
+    {
+        vis[node] = 1;
+        for(auto it : adj[node])
+        {
+            if(!vis[it])
+            {
+                a.pb(it);
+                dfs(a,it,adj,vis);
+            }
+        }
+
+        // return a;
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) 
+    {
+        vvi adj(n);
+        for(auto it : edges)
+        {
+            adj[it[1]].pb(it[0]);
+        }
+
+        vvi a(n);
+
+        f(n)
+        {
+            vi vis(n,0);
+            vi act;
+            dfs(act,i,adj,vis);
+            sort(act.begin(),act.end());
+            a[i] = act;
+        }
+
+        return a;
+
+    }
+};
